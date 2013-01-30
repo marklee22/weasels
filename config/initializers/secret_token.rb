708565ad4a794a609bfd2434370f99a428b54773
Mark Lee <marklee22@gmail.com>
2013-01-29 17:23:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weasels::Application.config.secret_token = '4b183b59ad909d5aea95f5af8eaaf753908164d045b85a23e85614f90e786adfc83b722c621313594582a3f10f7c81f16ee81fe883a55064d2f2ebd4ce4d0019'
